5a7a0ebcc9e11d7b45376fa85a6094d4e8397fb4
Ryan McGrath <ryan@venodesigns.net>
2009-05-06 02:20:50 -0400

    Renaming a function to make room for other trending search stuff

--- tango.py
+++ tango.py
@@ -9,7 +9,7 @@ class tango:
         # Authenticate here?
         self.twitter_user = twitter_user
     
-    def getUserTimeline(self, optional_count):
+    def getUserTimeline(self, count, page, since_id):
         userTimelineURL = "http://twitter.com/statuses/user_timeline/" + self.twitter_user + ".json" + ("" if optional_count is None else "?count=" + optional_count)
         userTimeline = simplejson.load(urllib2.urlopen(userTimelineURL))
         formattedTimeline = []
@@ -25,7 +25,7 @@ class tango:
             formattedTimeline.append(tweet['text'])
         return formattedTimeline
 
-    def getTrendingTopics(self):
+    def getCurrentTrends(self):
         # Returns an array of dictionary items containing the current trends
         trendingTopicsURL = "http://search.twitter.com/trends.json"
         trendingTopics = simplejson.load(urllib.urlopen(trendingTopicsURL))
