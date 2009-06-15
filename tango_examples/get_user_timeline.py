f0a27970f15151b233ce1fcea40338846c5298a1
Ryan McGrath <ryan@venodesigns.net>
2009-06-14 20:17:48 -0400

    Removed needless id parameter

--- tango_examples/get_user_timeline.py
+++ tango_examples/get_user_timeline.py
@@ -2,6 +2,6 @@ import tango
 
 # We won't authenticate for this, but sometimes it's necessary
 twitter = tango.setup()
-user_timeline = twitter.getUserTimeline(id=None, screen_name="ryanmcgrath")
+user_timeline = twitter.getUserTimeline(screen_name="ryanmcgrath")
 
 print user_timeline
