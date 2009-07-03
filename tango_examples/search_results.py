69f747a65881200afbdac4c6b6253dac66b347e4
Ryan McGrath <ryan@venodesigns.net>
2009-07-03 03:04:47 -0400

    Updated search example; major thanks to Sai for pointing out that this was broken.

--- tango_examples/search_results.py
+++ tango_examples/search_results.py
@@ -2,7 +2,7 @@ import tango
 
 """ Instantiate Tango with no Authentication """
 twitter = tango.setup()
-search_results = twitter.searchTwitter("WebsDotCom", "2")
+search_results = twitter.searchTwitter("WebsDotCom", rpp="50")
 
 for tweet in search_results["results"]:
 	print tweet["text"]
