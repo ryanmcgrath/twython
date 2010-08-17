d9fcd3a264edf62ec5b5a11c67f9d7ec3a740294
Randall Degges <rdegges@gmail.com>
2010-08-17 16:37:43 +0800

    Fixing setup.py script to use valid `setuptools` format.
    This will fix the broken pip / easy_install issues on many platforms (like
    most linux distros).

--- setup.py
+++ setup.py
@@ -1,46 +1,42 @@
 #!/usr/bin/python
 
 import sys, os
+from setuptools import setup
+from setuptools import find_packages
+
 
 __author__ = 'Ryan McGrath <ryan@venodesigns.net>'
 __version__ = '1.2'
 
-# Distutils version
-METADATA = dict(
-	name = "twython",
+
+setup(
+
+	# Basic package information.
+	name = 'twython',
 	version = __version__,
-	py_modules = ['setup', 'twython/__init__', 'twython/core', 'twython/twyauth', 'twython/streaming', 'twython/oauth'],
+	packages = find_packages(),
+
+	# Packaging options.
+	include_package_data = True,
+
+	# Package dependencies.
+	install_requires = ['setuptools', 'simplejson'],
+
+	# Metadata for PyPI.
 	author = 'Ryan McGrath',
 	author_email = 'ryan@venodesigns.net',
-	description = 'An easy (and up to date) way to access Twitter data with Python.',
-	long_description = open("README.markdown").read(),
 	license = 'MIT License',
 	url = 'http://github.com/ryanmcgrath/twython/tree/master',
 	keywords = 'twitter search api tweet twython',
-)
-
-# Setuptools version
-SETUPTOOLS_METADATA = dict(
-	install_requires = ['setuptools', 'simplejson'],
-	include_package_data = True,
+	description = 'An easy (and up to date) way to access Twitter data with Python.',
+	long_description = open('README.markdown').read(),
 	classifiers = [
 		'Development Status :: 4 - Beta',
 		'Intended Audience :: Developers',
 		'License :: OSI Approved :: MIT License',
 		'Topic :: Software Development :: Libraries :: Python Modules',
 		'Topic :: Communications :: Chat',
-		'Topic :: Internet',
+		'Topic :: Internet'
 	]
+
 )
-
-def Main():
-	try:
-		import setuptools
-		METADATA.update(SETUPTOOLS_METADATA)
-		setuptools.setup(**METADATA)
-	except ImportError:
-		import distutils.core
-		distutils.core.setup(**METADATA)
-
-if __name__ == '__main__':
-  Main()
