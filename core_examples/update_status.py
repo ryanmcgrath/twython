cf5b382d55090bdc654f371001320706475357b7
Remy D <remyd@civx.us>
2011-12-07 14:26:02 -0500

    PEP8 Edits: Removed Tabs, removed spaces around keywords, linebreaks in lines longer than 80 chars

--- core_examples/update_status.py
+++ core_examples/update_status.py
@@ -1,13 +1,14 @@
 from twython import Twython
 
 """
-	Note: for any method that'll require you to be authenticated (updating things, etc)
-	you'll need to go through the OAuth authentication ritual. See the example
-	Django application that's included with this package for more information.
+    Note: for any method that'll require you to be authenticated (updating
+    things, etc)
+    you'll need to go through the OAuth authentication ritual. See the example
+    Django application that's included with this package for more information.
 """
 twitter = Twython()
 
 # OAuth ritual...
 
 
-twitter.updateStatus(status = "See how easy this was?")
+twitter.updateStatus(status="See how easy this was?")
