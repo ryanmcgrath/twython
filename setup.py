072a257a1ffb1d9ee987f0a7c7b93127a22f6ec9
Mike Helmick <mikehelmick@me.com>
2012-05-30 11:35:30 -0300

    2.2.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 from setuptools import find_packages
 
 __author__ = 'Ryan McGrath <ryan@venodesigns.net>'
-__version__ = '2.1.0'
+__version__ = '2.2.0'
 
 setup(
     # Basic package information.
