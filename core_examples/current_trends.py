780a82ea518e5517a71c0074b40436d38046fbc4
Martin Sykora <martin.sykora@hotmail.co.uk>
2012-07-20 09:20:08 +0200

    example was out of date

--- core_examples/current_trends.py
+++ core_examples/current_trends.py
@@ -2,6 +2,10 @@ from twython import Twython
 
 """ Instantiate Twython with no Authentication """
 twitter = Twython()
-trends = twitter.getCurrentTrends()
+
+# twitter has now depreciated the API endpoint att getCurrentTrends()
+# instead use the trendsByLocation() method - see https://dev.twitter.com/docs/api/1/get/trends/current
+
+trends = twitter.trendsByLocation()
 
 print trends
