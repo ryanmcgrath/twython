cc31322102e0c2942713b8a98c1977644116d41d
Mike Helmick <mikehelmick@me.com>
2012-10-04 14:10:56 -0400

    Fixes #119

--- core_examples/public_timeline.py
+++
@@ -1,8 +0,0 @@
-from twython import Twython
-
-# Getting the public timeline requires no authentication, huzzah
-twitter = Twython()
-public_timeline = twitter.getPublicTimeline()
-
-for tweet in public_timeline:
-    print tweet["text"]
