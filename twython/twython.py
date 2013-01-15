a28febb0b4283be0d3d024bd9bd3709898d4a14c
Guru Devanla <grdvnl@gmail.com>
2013-01-15 16:43:50 -0600

    update error code and comment

--- twython/twython.py
+++ twython/twython.py
@@ -203,6 +203,7 @@ class Twython(object):
                 'error', 'An error occurred processing your request.')
             self._last_call['api_error'] = error_msg
 
+            #Twitter API 1.1 , always return 429 when rate limit is exceeded
             exceptionType = TwythonRateLimitError if response.status_code == 429 else TwythonError
 
             raise exceptionType(error_msg,
