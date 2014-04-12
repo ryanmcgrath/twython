f0d74f971a48546aaafbbd79fe9dd5295dc63758
Janez K <janez.kranjc@gmail.com>
2014-04-12 09:18:43 +0200

    also return headers with the content so that x-rate-limit-remaining can be accessed

--- twython/api.py
+++ twython/api.py
@@ -182,6 +182,7 @@ class Twython(EndpointsMixin, object):
         except ValueError:
             raise TwythonError('Response was not valid JSON. Unable to decode.')
 
+        content['headers']=dict(response.headers)
         return content
 
     def _get_error_message(self, response):
