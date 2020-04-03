b8d927df8eeff88717f3b7547461b637de87caf7
Ryan McGrath <ryan@rymc.io>
2020-04-02 23:08:52 -0700

    Kill 2.6/2.7

--- tests/config.py
+++ tests/config.py
@@ -2,10 +2,7 @@
 import os
 
 import sys
-if sys.version_info[0] == 2 and sys.version_info[1] == 6:
-    import unittest2 as unittest
-else:
-    import unittest
+import unittest
 
 app_key = os.environ.get('APP_KEY')
 app_secret = os.environ.get('APP_SECRET')
